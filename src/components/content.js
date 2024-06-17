09767b0fc9da6fd02ee01fc9a465905f1c6b8041
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-06-17 12:29:06 +1000

    FAWFAW

--- src/components/content.js
+++ src/components/content.js
@@ -1,39 +1,198 @@
+import { useState } from 'react';
+
 import '../style/index.css';
 import '../style/content.css';
-import Image from './image';
-import Questions from './questions';
-import Feed from './feed';
+
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 
 let Content = (props) => {
+    const formStartContainers = (questions_data) => {
+        let feed = [];
+        let conts = [];
+        questions_data.forEach(q_element => {
+            q_element.answers.forEach((a_element, index) => {
+                feed.push(
+                    {
+                        id: `card_${q_element.question_id}_${index}`, 
+                        img: a_element
+                    }
+                )
+            });
 
-    const get_image = (task_data) => {
-        return task_data["image_path"];
-    };
+            conts.push(
+                {
+                    id: `question_${q_element.question_id}`,
+                    text: q_element.text,
+                    items: []
+                }
+            );
+        })
+        
+        conts.push({
+            id: "feed",
+            items:  shuffle(feed)
+        });
+
+        return conts;
+    }
     
-    const get_questions = (task_data) => {
-        return task_data["questions"].map(e => {
-            let question_text = e["text"];
+    const check_answers = () => {
 
-            return question_text 
-        })
+
+    }
+
+    
+    let defaultConts = formStartContainers(props.task.questions);
+
+    const [draggedItem, setDraggedItem] = useState(null);
+    const [draggedFrom, setDraggedFrom] = useState(null);
+    const [containers, setContainers] = useState(defaultConts);
+    
+    const onDragStart = (event, containerIndex, itemIndex) => {
+        setDraggedItem(itemIndex);
+        setDraggedFrom(containerIndex);
+        event.dataTransfer.effectAllowed = 'move';
     };
 
-    const get_answers = (task_data) => {
-        return task_data["questions"].map(e => {
-            let answers = e["answers"];
-            return answers
-        })
+    const onDragOver = (event) => {
+        event.preventDefault();
     };
+
+    const onDrop = (event, containerIndex) => {
+        if (draggedFrom === null || draggedItem === null) return;
+
+        const draggedItemData = containers[draggedFrom].items[draggedItem];
+
+        if (draggedFrom === containerIndex) {
+            return;
+        }
+
+        let newContainers = [...containers];
+        newContainers[draggedFrom].items = newContainers[draggedFrom].items.filter((_, index) => index !== draggedItem);
+        newContainers[containerIndex].items = [...newContainers[containerIndex].items, draggedItemData];
+
+        setContainers(newContainers);
+        setDraggedItem(null);
+        setDraggedFrom(null);
+    };
+
+    const backToDefault = () => {
+        setContainers(defaultConts); 
+    }
 
     return (
         <>
             <div className={"content"}>
-                <Image image={ get_image(props.task) }/>
-                <Questions questions={ get_questions(props.task) }/>
-                <Feed answers={ get_answers(props.task) }/>
+
+                <div className={"plate image"}>
+                    <img src={ props.task.image_path } draggable='false'/>
+                </div>
+
+                <div className={"plate questions"}>
+                    <div className={"question_text archivo-black-800"}>
+                        { containers[0].text }
+                    </div>
+                    <div className={"question_answers"} key={ containers[0].id } onDragOver={ onDragOver } onDrop={(event) => onDrop(event, 0)}>
+                        { containers[0].items.map((item, index) => (
+                            <div 
+                                className={"plate card"} 
+                                key={item.id} 
+                                draggable 
+                                onDrag={ (event) => onDragStart(event, 0, index) }
+                            >
+                                <img src={item.img} draggable='false'/>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={"question_text archivo-black-800"}>
+                        { containers[1].text }
+                    </div>
+                    <div className={"question_answers"} key={ containers[1].id } onDragOver={ onDragOver } onDrop={(event) => onDrop(event, 1)}>
+                        { containers[1].items.map((item, index) => (
+                                <div 
+                                    className={"plate card"} 
+                                    key={item.id} 
+                                    draggable 
+                                    onDrag={ (event) => onDragStart(event, 1, index) }
+                                >
+                                    <img src={item.img} draggable='false'/>
+                                </div>
+                            ))}
+                    </div>
+                    <div className={"question_text archivo-black-800"}>
+                        { containers[2].text }
+                    </div>
+                    <div className={"question_answers"} key={ containers[2].id } onDragOver={ onDragOver } onDrop={(event) => onDrop(event, 2)}>
+                        { containers[2].items.map((item, index) => (
+                            <div 
+                                className={"plate card"} 
+                                key={item.id} 
+                                draggable 
+                                onDrag={ (event) => onDragStart(event, 2, index) }
+                            >
+                                <img src={item.img} draggable='false'/>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={"button "}>
+                        <input type='button' value={"Проверить"} className='archivo-black-800'></input>
+                        <input type='submit' value={"Очистить"} className='archivo-black-800' onClick={ backToDefault }></input>
+                    </div>
+                </div>
+
+                <div className={"plate feed"} id={ containers[3].id } onDragOver={ onDragOver } onDrop={(event) => onDrop(event, 3)}>
+                    {containers[3].items.map((item, index) => (
+                        <div 
+                            className={"plate card"} 
+                            key={item.id} 
+                            draggable 
+                            onDrag={ (event) => onDragStart(event, 3, index) }
+                        >
+                            <img src={item.img} draggable='false'/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
 };
+
+
+/*
+
+                    { containers.filter((container, container_index) => {
+                            if (container_index < containers.length - 1) {
+                                return (
+                                    <>
+                                        <div className={"question_text archivo-black-800"}>
+                                            { container.text }
+                                        </div>
+                                        <div className={"question_answers"} key={ container.id } onDragOver={ onDragOver } onDrop={(event) => onDrop(event, container_index)}>
+                                            { container.items.map((item, index) => (
+                                                <div 
+                                                    className={"plate card"} 
+                                                    key={item.id} 
+                                                    draggable 
+                                                    onDrag={ (event) => onDragStart(event, container_index, index) }
+                                                >
+                                                    <img src={item.img} draggable='false'/>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </>
+                                )
+                            }
+                        })
+                    }
+*/
+
 
 export default Content;
--- src/data/data.js
+++ src/data/data.js
@@ -1,26 +1,26 @@
 let question_list = [
     {
-        "image_path": `${process.env.PUBLIC_URL}/images/task_1/task.png`,
-        "questions": [
+        image_path: `${process.env.PUBLIC_URL}/images/task_1/task.png`,
+        questions: [
             {   
-                "question_id": 1,
-                "text": "Формула нахождения площади:",
-                "answers": [
+                question_id: 1,
+                text: "Формула нахождения площади:",
+                answers: [
                     `${process.env.PUBLIC_URL}/images/task_1/1.png`, 
                 ]
             },
             {
-                "question_id": 2,
-                "text": "Формула нахождения угла А:",
-                "answers": [
+                question_id: 2,
+                text: "Формула нахождения угла А:",
+                answers: [
                     `${process.env.PUBLIC_URL}/images/task_1/2.png`,
                     `${process.env.PUBLIC_URL}/images/task_1/3.png`,
                 ]
             },
             {
-                "question_id": 3,
-                "text": "Формула нахождения периметра:",
-                "answers": [
+                question_id: 3,
+                text: "Формула нахождения периметра:",
+                answers: [
                     `${process.env.PUBLIC_URL}/images/task_1/4.png`,
                     `${process.env.PUBLIC_URL}/images/task_1/5.png`,
                     `${process.env.PUBLIC_URL}/images/task_1/6.png`
